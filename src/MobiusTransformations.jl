f60795f9d3f7b0fc2c5a535d97e2e7dc7fdcbf05
LauBMo <laurea987@gmail.com>
2025-10-02 16:34:36 +0200

    Compiles and works...

--- src/MobiusTransformations.jl
+++ src/MobiusTransformations.jl
@@ -1,5 +1,277 @@
 module MobiusTransformations
 
-# Write your package code here.
+# import LinearAlgebra: det
+using UnPack: @unpack
 
+import Base: inv, show, hash, ==, *, ∘, isone, typeof
+
+export Mobius, Möbius, stereo
+
+include("StereographicProjections.jl")
+
+# Tolerance constants
+const NUM_TOL = 1e-12 # for printing,...
+const SPHERE_TOL = 1e-8 # for detect north pole
+
+
+# Infinite to completing complaxe plane.
+const INF = Ref{Any}(complex(Inf))
+
+"""
+    set_infinity(infinity)
+
+Sets the representation of infinity used by the package.
+"""
+function set_infinity(infinity)
+    INF[] = infinity
 end
+
+
+# Möbius transformation
+"""
+    MobiusTransformation{T}
+
+Represents a Möbius transformation of the form:
+f(z) = (a*z + b) / (c*z + d)
+
+# Fields
+- `a::T, b::T, c::T, d::T`: Coefficients of the transformation
+"""
+struct MobiusTransformation{T} <: Function
+    a::T
+    b::T
+    c::T
+    d::T
+end
+
+# Dealing with diferent types
+"""
+    MobiusTransformation(a, b, c, d)
+
+Creates a Möbius transformation with coefficients a, b, c, d.
+"""
+MobiusTransformation(a, b, c, d) = MobiusTransformation(promote(a, b, c, d)...)
+
+# From array-alike objects
+"""
+    MobiusTransformation(A)
+
+Creates a Möbius transformation from an array or tuple of coefficients [a, b, c, d].
+"""
+MobiusTransformation(A) = MobiusTransformation(A...)
+
+
+# Computing Mobius transformations
+
+# Möbius(...) calls:
+# 4 args: Build MobiusTransformation (generic)
+# 3 args: Target Number-like -> Standard transformation
+# 6 args: Transformation source Number-like -> target Number-like
+# 1 args: Target vector -> 3 args
+# 2 args: Source, Target vectors -> 6 args
+# 1 args: arg is a Type, returns Identity (default ComplexF64)
+# 1 args: arg is a Matrix Q, returns "rotation by Q".
+
+"""
+    Möbius(a, b, c, d)
+
+Creates a Möbius transformation with coefficients a, b, c, d.
+"""
+Möbius(a, b, c, d) = MobiusTransformation(a, b, c, d)
+
+const Mobius = Möbius   # for us lazy Americans
+
+"""
+    Möbius(x, y, z)
+
+Returns the Möbius transformation that maps `[0, 1, Inf]` to given points.
+Values of `Inf` are permitted.
+"""
+function Möbius(x, y, z)
+    if isinf(x)
+        return z, y - z, 1, 0
+    elseif isinf(y)
+        return -z, x, -1, 1
+    elseif isinf(z)
+        return y - x, x, 0, 1
+    else
+        xy, yz = y - x, z - y
+        return MobiusTransformation(z * xy, x * yz, xy, yz)
+    end
+end
+
+"""
+    Möbius(x, y, z, X, Y, Z)
+
+Returns the Möbius transformation that maps `[x, y, z]` to `[X, Y, Z]`.
+Values of `Inf` are permitted.
+"""
+function Möbius(x, y, z, X, Y, Z)
+    m_source = Möbius(x, y, z) # (0, 1, Inf) -> (x, y, z)
+    m_image = Möbius(X, Y, Z) # (0, 1, Inf) -> (X, Y, Z)
+    # No division performed.
+    return m_image * inv(m_source) # (x, y, z) -> (X, Y, Z)
+end
+
+"""
+    Möbius(Q::AbstractMatrix, source=[0, 1, im])
+
+Given a 3D rotation `Q` (so, `Q*Q'=Id` and `det(Q)=1`), returns the Mobius transformation `m` corresponding to rotate the standard Riemann sphere by Q.
+That is, the map `m` is defined as `m(z) = p(Q*p(z))`, where `p = stereo()` is the standard stereographic projection.
+"""
+function Möbius(Q::AbstractMatrix, source=[0, 1, im])
+    p = stereo()
+    # We can set any source points.
+    # Compute target points.
+    source_sphere = p.(source)
+    target_sphere = [Q * P for P in source_sphere]
+    target = p.(target_sphere)
+
+    return Möbius(source, target)
+end
+
+"""
+    Möbius(target)
+
+Returns the Möbius transformation that maps `[0, 1, Inf]` to given `target = [z1, z2, z3]`.
+Values of `Inf` are permitted.
+"""
+Möbius(target) = Möbius(target...)
+
+
+"""
+    Möbius(source, target)
+
+Returns the Möbius transformation that maps `source` to `target`.
+Values of `Inf` are permitted.
+"""
+Möbius(source, target) = Möbius(source..., target...)
+
+"""
+    Möbius(::Type{T}=ComplexF64)
+
+Returns the identity Möbius transformation of type `T`.
+"""
+Möbius(::Type{T}=ComplexF64) where T = Möbius(one(T), zero(T), zero(T), one(T))
+
+"""
+    isone(m::MobiusTransformation)
+
+Return `true` if `m` is the identity Mobius transformation and `false` otherwise.
+"""
+function isone(m::MobiusTransformation)
+    @unpack a, b, c, d = m
+    return iszero(b) && iszero(c) && (a == d)
+end
+==(m::MobiusTransformation, n::MobiusTransformation) = isone(m * inv(n))
+
+# TODO
+# ≈(m::MobiusTransformation, n::MobiusTransformation) = isapproxone(m*inv(n))
+#
+
+Base.eltype(_::MobiusTransformation{T}) where {T} = T
+
+function Base.hash(m::MobiusTransformation, h::UInt64=UInt64(0))
+    z = 0.0 + 0.0 * im # kludge to make -0.0 and -0.0im into +versions
+    a = m(0) + z
+    b = m(1) + z
+    c = m(Inf) + z
+    return hash(a, hash(b, hash(c, h)))
+end
+
+# Vectorized operations
+Base.broadcastable(m::MobiusTransformation) = Ref(m)
+
+function det(m::MobiusTransformation)
+    @unpack a, b, c, d = m
+    return a * d - b * c
+end
+
+"""
+    normalize(m::MobiusTransformation)
+
+Returns a Mobius transformation `m2` such that `m2 == m1` and `det(m2) = 1`.
+"""
+normalize(m::MobiusTransformation) = inv(det(m)) * m
+
+function Base.Matrix(m::MobiusTransformation)
+    @unpack a, b, c, d = m
+    return [a b; c d]
+end
+
+# Inverse Möbius transformation
+function Base.inv(m::MobiusTransformation)
+    @unpack a, b, c, d = m
+    MobiusTransformation(d, -b, -c, a)
+end
+
+*(λ, m::MobiusTransformation) = MobiusTransformation(λ * Matrix(m))
+*(m::MobiusTransformation, λ) = *(λ, m)
+
+"""
+    *(m::MobiusTransformation, n::MobiusTransformation)
+
+Compose two Möbius transformations.
+"""
+*(m::MobiusTransformation, n::MobiusTransformation) =
+    MobiusTransformation(Matrix(m) * Matrix(n))
+
+"""
+    ∘(m::MobiusTransformation, n::MobiusTransformation)
+
+Compose two Möbius transformations (same as *).
+"""
+∘(m::MobiusTransformation, n::MobiusTransformation) = m * n
+
+# Apply Möbius transformation
+"""
+    (m::MobiusTransformation)(z)
+
+Apply to a complex number z the Möbius transformation
+`m(z) = (a*z + b) / (c*z + d)`, where `m = Mobius([a b; c d])`.
+"""
+function (m::MobiusTransformation)(z)
+    @unpack a, b, c, d = m
+    if isinf(z)
+        numer, denom = a, c
+    else
+        numer, denom = a * z + b, c * z + d
+    end
+
+    if abs(denom) == 0
+        return INF[]
+    else
+        return numer * inv(denom)
+    end
+end
+
+#
+# Display
+#
+
+function show(io::IO, m::MobiusTransformation)
+    @unpack a, b, c, d = m
+    print(IOContext(io, :compact => true), "Möbius map z --> ($a*z + $b) / ($c*z + $d)")
+end
+
+function show(io::IO, ::MIME"text/plain", m::MobiusTransformation)
+    string_linear((X, Y)) = "(" * X * ")*z + " * Y
+    @unpack a, b, c, d = m
+    # if abs(c) < NUM_TOL
+    #     A, B = [repr("text/plain", x) for x in [a*inv(d), b*inv(d)]]
+    #     numer = string_linear((A, B))
+    #     print(io, "Möbius:\n   ", numer)
+    # end
+    A, B, C, D = [repr("text/plain", x) for x in [a, b, c, d]]
+    numer, denom = string_linear.([(A, B), (C, D)])
+    newline = "\n   "
+    hline = reduce(*, fill("–", maximum(length, [numer, denom])))
+
+    ## Print message
+    print(io, "Möbius: ", eltype(m), newline,
+          numer, newline,
+          hline, newline,
+          denom)
+end
+
+end # of module MobiusTransformations.
---
+++ src/StereographicProjections.jl
@@ -0,0 +1,93 @@
+# Stereographic projections
+struct StereographicProjection{T}
+    # sphere center coordinates
+    x::T
+    y::T
+    z::T
+end
+
+stereo(v) = StereographicProjection{eltype(v)}(v...)
+stereo_projection(v) = stereo(v)
+stereo() = stereo(zeros(Float64, 3))
+
+# Helper function: get the center of the sphere we are projecting from.
+vec(proj::StereographicProjection) = [proj.x, proj.y, proj.z]
+
+# Helper function: compute north pole (focus of the projection)
+const NorthAxis = 2 # y for POV-Ray
+const OtherAxis = filter(x -> !(x == NorthAxis), 1:3)
+const NorthDir = begin
+    out = zeros(Float64, 3)
+    out[NorthAxis] = 1.0
+    out
+end
+
+north_pole(proj::StereographicProjection) = vec(proj) + NorthDir
+
+# Project sphere point to complex plane
+function (proj::StereographicProjection)(P::AbstractVector{T}) where {T}
+    np = north_pole(proj)
+    dir = P - np
+
+    # Handle north pole (no dir)
+    if sum(abs2, dir) == 0
+        return INF[]
+    end
+
+    # Compute parameter t using plane equation
+    i = NorthAxis
+    t = -np[i] * inv(dir[i])
+
+    # Project to plane
+    j, k = OtherAxis
+    x_proj = np[j] + t * dir[j]
+    y_proj = np[k] + t * dir[k]
+
+    _complex(x_proj, y_proj)
+end
+# To deal with CalciumFieldElem, or Nemo shit in general.
+_complex(x, y) = x + y * parent(x)(1*im)
+_complex(x::Number, y::Number) = complex(x, y)
+
+# So tempting... but type piracy.
+# function Base.*(a::CalciumFieldElem, z::Complex{T}) where {T}
+#     C = a.parent
+#     x, y = reim(z)
+#     return a*x + a*y*onei(C)
+# end
+
+# Project complex number to sphere
+function (proj::StereographicProjection)(z)
+    np = north_pole(proj)
+
+    # Handle infinity
+    if isinf(z)
+        return np
+    end
+
+    # Unpack complex number
+    x, y = reim(z)
+    pz = [zero(x), zero(x), zero(x)]
+    j, k = OtherAxis
+    pz[j] = x
+    pz[k] = y
+    # P_plane = [x, y, zero(T)]
+    # dir = P_plane - np
+
+    # Direction vector
+    # Do not need of type 'T'
+    dir = pz - np
+
+    # Solve quadratic equation for t
+    ## b = 2*dot(dir, np - center)
+    ## But, we have np-c = [0,0,1], so
+    i = NorthAxis
+    A = 2 * dir[i]
+    B = sum(abs2, dir)
+
+    # Use the non-zero solution
+    t = -A * inv(B)
+
+    # Calculate intersection point
+    return np + t .* dir
+end
